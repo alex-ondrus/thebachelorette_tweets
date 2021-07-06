ab1e15c011d9b36c3f4b77becfbb02a77558140a
Alex Ondrus <aondrus@me.com>
2021-07-06 15:28:19 -0600

    First draft up to plotting function

---
+++ code/mine_tweets.R
@@ -0,0 +1,76 @@
+##### Load libraries and data #####
+
+library(tidyverse)
+library(tidytext)
+library(magrittr)
+library(ggrepel)
+library(ggthemes)
+
+afinn_sent <- get_sentiments("afinn")
+
+##### Analyze sentiment #####
+
+words_w_sentiment <- function(episode_tweets_w_mins){
+  sents <- episode_tweets_w_mins %>% 
+    select(text, minutes_in) %>% 
+    unnest_tokens(word, text) %>% 
+    inner_join(afinn_sent)
+  return(sents)
+}
+
+sentiment_by_minute <- function(sents){
+  sent_by_minute <- sents %>% 
+    group_by(minutes_in) %>% 
+    summarise(minute_sent = sum(value)) %>% 
+    mutate(cumulative_sent = cumsum(minute_sent))
+}
+
+popular_sentimental_words <- function(sents){
+  minute_words <- sents %>% 
+    group_by(minutes_in, word) %>% 
+    summarise(word_count = n())
+  
+  max_counts <- minute_words %>% 
+    summarise(max_count = max(word_count))
+  
+  minute_words %<>% 
+    left_join(max_counts) %>% 
+    mutate(is_max = (max_count == word_count)) %>% 
+    ungroup() %>% 
+    filter(is_max) %>% 
+    group_by(minutes_in) %>% 
+    summarise(most_popular = paste0(word, collapse = ", "))
+}
+
+build_plot_df <- function(episode_tweets_w_mins){
+  sentiments <- words_w_sentiment(episode_tweets_w_mins)
+  plot_df <- inner_join(sentiment_by_minute(sentiments),
+                    popular_sentimental_words(sentiments)) %>% 
+    mutate(label_point = (abs(cume_dist(test_plot$minute_sent) - 0.5) > 0.48),
+           point_alpha = ifelse(label_point, 1, 0),
+           point_label = ifelse(label_point, most_popular, NA_character_))
+}
+
+##### Generate visuals #####
+
+generate_sent_plot <- function(plot_df){
+  sent_plot <- ggplot(plot_df,
+                      aes(x = minutes_in,
+                          y = cumulative_sent)) +
+    geom_point(aes(alpha = point_alpha)) +
+    geom_label_repel(aes(label = point_label),
+                     alpha = 0.75) +
+    geom_line() +
+    scale_alpha(guide = "none") +
+    labs(title = "Tell Us How You Really Feel",
+         subtitle = "Cumulative sentiment of words tweeted with hashtag #theBachelorette.\nLabels show most common 'sentimental' words.",
+         x = "Minutes Into Episode",
+         y = "Cumulative Sentiment (AFINN)",
+         caption = "Data: Twitter API\nGraph: @DrAOndrus") +
+    theme_hc()
+  ggsave("images/sent_by_min.jpeg",
+         width = 6,
+         height = 4, 
+         dpi = 600)
+  return(sent_plot)
+}
--- code/pull_web_data.R
+++ code/pull_web_data.R
@@ -2,11 +2,40 @@
 
 library(rtweet)
 library(tidyverse)
+library(lubridate)
 
-##### Obtain and filter data #####
+##### Obtain data #####
+
+# This function will only pull tweets during the last 7 days, so if run within 7 days of
+# the episode in interest ending, you should catch everything
 
 get_bach_tweets <- function(hashtag){
   bach_tweets <- search_tweets(hashtag, n = 18000)
   write_as_csv(bach_tweets, paste("tweets_", Sys.Date(), ".csv", sep = ""))
   return(bach_tweets)
 }
+
+##### Filter data #####
+
+# Filter for the tweets during the episode start and end in Eastern time
+# e.g. episode_start = ymd_hms("2021-07-05 20:00:00", tz = "America/Toronto")
+
+episode_tweet_text_time <- function(bach_tweets,
+                                    episode_start,
+                                    episode_end){
+  episode_tweets <- bach_tweets %>% 
+    filter(created_at <= episode_end,
+           created_at >= episode_start) %>% 
+    select(created_at, text) %>% 
+    mutate(seconds_in = created_at - episode_start,
+           minutes_in = round(as.numeric(seconds_in/60)))
+  
+  return(episode_tweets)
+}
+
+##### Egs for debugging #####
+
+# bach_tweets <- get_bach_tweets("#theBachelorette")
+# episode_tweet_text <- episode_tweet_text_time(bach_tweets,
+#                                               ymd_hms("2021-07-05 20:00:00", tz = "America/Toronto"),
+#                                               ymd_hms("2021-07-05 22:00:00", tz = "America/Toronto"))
